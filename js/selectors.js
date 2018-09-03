0ea870f1b2d40faa2350fc47a51bcffbd02e0d0f
Stéphane <stephluethi@hotmail.com>
2018-09-03 13:45:36 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,20 +1,20 @@
 // declare your functions here...
 function paragraphSelector() {
-  $('p')
+  $('p');
 }
 
 function lastImageSelector() {
-  $('img:last')
+  $('img:last');
 }
 
 function ninjaBabySelector() {
-  $('#baby-ninja')
+  $('#baby-ninja');
 }
 
 function divSelector() {
-  $('.pics')
+  $('.pics');
 }
 
 function firstListItem() {
-  $('ul#pic-list li:first-child')
+  $('ul#pic-list li:first-child');
 }
